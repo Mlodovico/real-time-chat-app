3aac7f8a6aee4fe140bf6adb7df6a18de01c5240
Mlodovico <murilo.lodovico@outlook.com>
2025-05-04 11:47:12 -0300

    feature: websocket backend configuration

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	mux := http.NewServeMux()
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 	
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	if err != nil {
+		log.Println("Error upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+
+		log.Printf("Received message: %s", msg)
+
+		err = conn.WriteMessage(messageType, msg)
+		if err != nil {
+			log.Println("Error writing message:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleWebSocket)
+	fmt.Println("WebSocket server started on :8080")
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
