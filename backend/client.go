f7261cfa1e368d08d360b32ad764df22b71d8e5a
Mlodovico <murilo.lodovico@outlook.com>
2025-06-30 00:03:42 -0300

    feature: resolving server problem

---
+++ backend/client.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+		return true;
+	}, // Allow all origins for simplicity; adjust as needed for security
+}
+
+func ServeWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("Error upgrading connection:", err)
+        return
+    }
+    client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+    client.hub.register <- client
+
+    go client.writePump()
+    go client.readPump()
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			c.conn.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -4,45 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	
-	if err != nil {
-		log.Println("Error upgrading connection:", err)
-		return
-	}
-	defer conn.Close()
-
-	for {
-		messageType, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Error reading message:", err)
-			break
-		}
-
-		log.Printf("Received message: %s", msg)
-
-		err = conn.WriteMessage(messageType, msg)
-		if err != nil {
-			log.Println("Error writing message:", err)
-			break
-		}
-	}
-}
-
 func main() {
-	http.HandleFunc("/ws", handleWebSocket)
+	hub := NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		ServeWs(hub, w, r)
+	})
+
 	fmt.Println("WebSocket server started on :8081")
-	
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
